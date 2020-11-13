aac2f59268ffc2bdde93d162ce53d0355e70fcad
Frederik Leys <frederik.leys@icapps.com>
2020-11-13 14:53:58 +0100

    correctly implemented, proper refactoring still required

--- flutter-template-master/lib/viewmodel/barcodeScanner/barcode_scanner_viewmodel.dart
+++ flutter-template-master/lib/viewmodel/barcodeScanner/barcode_scanner_viewmodel.dart
@@ -1,41 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class BarcodeScannerViewModel with ChangeNotifier {
   BarcodeScannerNavigator _navigator;
   String _scanBarcode = 'Unknown';
-
   String get scannedBarcode => _scanBarcode;
 
   Future<void> init(BarcodeScannerNavigator navigator) async {
     _navigator = navigator;
   }
 
-  // Future<void> requestPermission(Permission permission) async {
-  //   final status = await permission.request();
-  //
-  //   setState(() {
-  //     print(status);
-  //     _permissionStatus = status;
-  //     print(_permissionStatus);
-  //   });
-  // }
-
   Future<void> onStartBarcodeScan() async {
-    //requestPermission(_permission);
-
-    String _barcodeScanRes;
-    try {
-      _barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#ff6666', 'Cancel', true, ScanMode.BARCODE);
-    } on PlatformException {
-      _barcodeScanRes = 'Failed to get platform version';
+    if (await Permission.camera.request().isGranted) {
+      String _barcodeScanRes;
+      try {
+        _barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#ff6666', 'Cancel', true, ScanMode.BARCODE);
+      } on PlatformException {
+        _barcodeScanRes = 'Failed to get platform version';
+      }
+      _scanBarcode = _barcodeScanRes;
     }
 
-    /// TODO: What does 'mounted' mean and is it required here? (Originally in screen and with setState)
-    //if (!mounted) return;
-
-    _scanBarcode = _barcodeScanRes;
     notifyListeners();
   }
 }
